ce51a1abb1a9807bd2e80ce9874a40bf36aff2fe
Laís Lorrany <lorranyand@gmail.com>
2015-11-10 01:24:47 -0200

    Versão MultiThread

---
+++ src/Calcula.java
@@ -0,0 +1,64 @@
+
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Calcula {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GerenciaArquivo gArquivo = new GerenciaArquivo();
+		int result = 0;
+		String ar;
+		String conteudo = "";
+		
+		/*
+		 * Leitura do arquivo entrada.txt para identificar qual a sentença a ser realizada.
+		 * */
+		while(true){
+			
+			ar = gArquivo.leituraSentenca();
+			gArquivo.limpaArquivo(1);
+			calculo(ar, gArquivo);
+	
+		}
+	}
+	
+	public static void calculo(String conteudo, GerenciaArquivo gArquivo){
+
+		try{		
+
+			int a = 0, b = 0;
+			Operacao operacao = new Operacao();
+			char op = ' ';
+			String [] cont;
+			int result = 0;
+			
+			cont = conteudo.split(" ");
+			a = Integer.parseInt(cont[0]);
+			op = cont[1].charAt(0);
+			b = Integer.parseInt(cont[2]);
+			
+			switch (op){
+				case '+' :	result = operacao.Somar(a, b);
+					break;
+				case '-' :	result = operacao.Subtrair(a, b);
+					break;
+				case '*' :	result = operacao.Multiplicar(a, b);
+					break;
+				case '/' :	result = operacao.Dividir(a, b);
+					break;
+				default :	System.out.println("Operação incorreta.");
+					break;
+			}
+			
+			gArquivo.escreveResultado(result);
+			
+		} catch(Exception e){
+			System.out.println("Erro Calculo:" + e.getMessage());
+		}		
+		
+	}
+
+}
---
+++ src/Operacao.java
@@ -0,0 +1,25 @@
+
+
+public class Operacao {
+	
+	public int Somar (int a, int b){
+		System.out.println("+");
+		return a + b;
+	}
+	
+	public int Subtrair (int a, int b){
+		System.out.println("-");
+		return a - b;
+	}
+	
+	public int Multiplicar (int a, int b){
+		System.out.println("*");
+		return a * b;
+	}
+	
+	public int Dividir (int a, int b){
+		System.out.println("/");
+		return a / b;
+	}
+	
+}
